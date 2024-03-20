3a429196729d8205923907578a09b1d0413b29aa
Twanst Codes <155393812+TwanstCodess@users.noreply.github.com>
2024-03-21 00:42:11 +0300

    Navbar

--- resources/js/layouts/layout.vue
+++ resources/js/layouts/layout.vue
@@ -4,10 +4,97 @@ import { Link } from "@inertiajs/vue3";
 
 <template>
     <div>
-        <Link href="/">Home</Link>
-        <Link href="/contact">Contact</Link>
-        <Link href="/about">About</Link>
+        <nav class="bg-gray-800">
+            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div class="flex items-center justify-between h-16">
+                    <div class="flex items-center">
+                        <div class="flex-shrink-0">
+                            <Link href="/" class="text-white"
+                                >Inertia Js And Laravel</Link
+                            >
+                        </div>
+                        <div class="hidden md:block">
+                            <div class="ml-10 flex items-baseline space-x-4">
+                                <Link
+                                    href="/"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >Home</Link
+                                >
+                                <Link
+                                    href="/about"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >About</Link
+                                >
+
+                                <Link
+                                    href="/contact"
+                                    class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                    >Contact</Link
+                                >
+                            </div>
+                        </div>
+                    </div>
+                    <div class="-mr-2 flex md:hidden">
+                        <button
+                            @click="open = !open"
+                            class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700 focus:text-white"
+                            aria-controls="mobile-menu"
+                            :aria-expanded="open ? 'true' : 'false'"
+                        >
+                            <svg
+                                class="h-6 w-6"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    :x-show="!open"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M4 6h16M4 12h16M4 18h16"
+                                />
+                                <path
+                                    :x-show="open"
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M6 18L18 6M6 6l12 12"
+                                />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div
+                :class="{ block: open, hidden: !open }"
+                class="md:hidden"
+                id="mobile-menu"
+            >
+                <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                    <Link
+                        href="/"
+                        class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >Home</Link
+                    >
+                    <Link
+                        href="/about"
+                        class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >About</Link
+                    >
+
+                    <Link
+                        href="/contact"
+                        class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                        >Contact</Link
+                    >
+                </div>
+            </div>
+        </nav>
     </div>
-    <slot />
-    <div></div>
+    <div class="container mx-auto py-4">
+        <slot> </slot>
+    </div>
 </template>
